7faa33012ebe748a8e6370cf7aa5b8a14ce0f086
Alex Do <baolocdo@users.noreply.github.com>
2020-10-18 21:11:44 -0700

    Create hello-world.test.js

---
+++ hello-world.test.js
@@ -0,0 +1,13 @@
+const sayHello = require('./sayHello');
+
+test('should returns \'A says "Hello!" to B\'', () => {
+  expect(sayHello('A', 'B')).toBe('A says "Hello!" to B');
+});
+
+test('should returns \'Alice says "Hello!" to Bob\'', () => {
+  expect(sayHello('Alice', 'Bob')).toBe('Alice says "Hello!" to Bob');
+});
+
+test('should returns \' says "Hello!" to \'', () => {
+  expect(sayHello('', '')).toBe(' says "Hello!" to ');
+});
