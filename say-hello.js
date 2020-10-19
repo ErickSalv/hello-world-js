4bc2752ae5ce19e2506c708552299ea32b1e5d57
Alex Do <baolocdo@users.noreply.github.com>
2020-10-18 20:59:38 -0700

    Update say-hello.js

--- say-hello.js
+++ say-hello.js
@@ -1,5 +1,10 @@
+// Returns a string '<fromPerson> says "Hello!" to <toPerson>'.
+// @param fromPerson The name of the person that says "Hello!".
+// @param toPerson The name of the person that the other person says "Hello!" to.
 function sayHello(fromPerson, toPerson) {
-  return '';
+  // TODO: Finish the right side of the next line. Do not change anything else.
+  let result = '';
+  return result;
 }
 
 sayHello('Alice', 'Bob'); // Should return 'Alice says "Hello!" to Bob'
